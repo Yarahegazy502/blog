5436606c587dbc78d30206cea483027c65062fc9
Yarahegazy502 <61470818+Yarahegazy502@users.noreply.github.com>
2021-04-26 20:47:50 +0200

    blog template

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// $('.owl-carousel').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+
+$('.owl-carousel').slick({
+    loop:true,
+    margin:10,
+    speed:500,
+    nav:true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
